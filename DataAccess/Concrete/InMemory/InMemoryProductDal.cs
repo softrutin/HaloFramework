cb84e82f31bd27cfdfb9f1215bb49e17aed2f29d
halilgoceri <halil.goceri@net-bt.com.tr>
2021-02-23 00:14:15 +0300

    add product service,dal,entity

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,36 @@
+﻿using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategoryId(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Entity/Concrete/Product.cs
@@ -0,0 +1,16 @@
+﻿using Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity.Concrete
+{
+    public class Product:IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public int UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
