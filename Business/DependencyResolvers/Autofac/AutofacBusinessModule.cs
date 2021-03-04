1b2784b8f122fda50167c8c88a83ddc718643009
halilgoceri <halil.goceri@net-bt.com.tr>
2021-03-05 00:39:58 +0300

    5.03.2021-2

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -15,7 +15,9 @@ namespace Business.DependencyResolvers.Autofac
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
             builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
             builder.RegisterType<DatabaseLogger>().As<ILogger>().SingleInstance();
 
 
